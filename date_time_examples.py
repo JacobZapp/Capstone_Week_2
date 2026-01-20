dad295b2a9a6b9ab5321ae74dcbe88ff2d674d88
Jacob Zapp <jbzapp01@gmail.com>
2026-01-20 11:05:29 -0600

    datetime and tuples video code

--- date_time_examples.py
+++ date_time_examples.py
@@ -1,2 +1,23 @@
  # This import the correct things related to dates and times, neccessary for anything to do with dates or times
 from datetime import datetime, date, time
+
+
+# Getting curent date and storing in variable
+today = date.today()
+#By default, printing a date will show it in the format YYYY-MM-DD
+print(today)
+
+tomorrow = date(2026, 1, 21) # This date format is called ISO format
+print(tomorrow)  # Prints 2026-01-21
+
+# You can create a date from string format using the isoformat method
+next_week = date.fromisoformat("2026-01-28")
+print(next_week)
+
+right_now = datetime.now()  # This gets the current date and time
+print(right_now)
+
+print(right_now.timestamp()) # using timestamp method to get number of seconds since epoch
+
+my_date = datetime.fromtimestamp(1700000000) # Creating a datetime object from a timestamp
+print(my_date)
---
+++ tuples.py
@@ -0,0 +1,41 @@
+# The pairs here are what tuples are. Number of items you can fit into a tuple is infinite, but usually they are small in size.
+city_state = [('Minneapolis', 'MN'), ('Baraboo', 'WI'), ('New York', 'NY'), ('Los Angeles', 'CA'), ('Miami', 'FL')]
+print(len(city_state))
+
+# Getting first tuple from the list
+first_pair = city_state[0]
+print(first_pair)
+
+# Tuple Unpacking
+print(first_pair[0])  # Accessing first element of the tuple
+print(first_pair[1])  # Accessing second element of the tuple 
+
+city, state = first_pair  # Unpacking the tuple into two variables
+print(city) # Accessing city
+print(state) # Accessing state
+print(city, state) # Printing both city and state
+
+# Example of more than 2 items in a tuple being unpacked
+animals = [('lion', 'tiger', 'cougar'), ('black bear', 'grizzly bear', 'polar bear'), ('eagle', 'hawk', 'falcon')]
+
+
+lion, tiger, cougar = animals[0]  # Unpacking the first tuple in the list
+print(lion)
+print(tiger)
+print(cougar)
+
+# Getting Distance Function
+def get_distance():
+    miles = 1000
+    km = miles * 1.6
+    return miles, km  # Returning a tuple with two values
+
+distances = get_distance()  # Receiving the tuple
+print(distances)  # Printing the tuple
+
+miles, km = get_distance()  # Unpacking the returned tuple into two variables
+print(miles)  # Printing miles
+print(km)     # Printing kilometers
+
+
+
